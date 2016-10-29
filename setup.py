b2da6df3aa26e88138d747862cc5c88c6a3a5202
Aaron Adel <aadel112@gmail.com>
2016-10-29 23:02:16 +0000

    setup structure + added simulated partiton

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
-config = {
-    'description': 'My Project',
-    'author': 'My Name',
-    'url': 'URL to get it at.',
-    'download_url': 'Where to download it.',
-    'author_email': 'My email.',
-    'version': '0.1',
-    'install_requires': ['nose'],
-    'packages': ['NAME'],
-    'scripts': [],
-    'name': 'projectname'
-}
+with open('README.rst') as f:
+    readme = f.read()
 
-setup(**config)
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='oPiUS',
+    version='0.0.1',
+    description='A simple peak calculator using python and sqlite in-memory for fast ranged-index based searches',
+    long_description=readme,
+    author='Aaron Adel',
+    author_email='aadel112@gmail.com',
+    url='https://github.com/aadel112/oPiUS',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
