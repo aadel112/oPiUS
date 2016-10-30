6d7ffa63b60f4b46f975c8d971c1c5fd57d4608a
Aaron Adel <aadel112@gmail.com>
2016-10-30 02:56:10 +0000

    added basic tests

--- tests/opius_tests.py
+++ tests/opius_tests.py
@@ -1,11 +1,23 @@
 from nose.tools import *
-import opius
+from .context import oPiUS
+import sys
+
+#     opius = oPiUS(in_h, ide, sc, ec, ot, o_h)
+#     opius.load()
+#     opius.find_peaks()
+#     opius.output()
+#     opius.close()
+
+o = oPiUS.opius.oPiUS('examples/ex1')
 
 def setup():
-    print "SETUP!"
+    o.load()
+    o.find_peaks()
 
 def teardown():
-    print "TEAR DOWN!"
+    o.close()
 
 def test_basic():
-    print "I RAN!"
+    assert o <> None
+    assert len(list(o.peaks.keys())) > 0
+
